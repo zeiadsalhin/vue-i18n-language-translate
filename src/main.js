b7587b168f2d0ec5e366c6f8e17edbb05fec24a9
zeiadsalhin <zeyadali011@gmail.com>
2024-02-14 04:33:18 +0200

    v1

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createI18n } from 'vue-i18n'
-// import ajax from "vuejs-ajax"
+
 
 const i18n = createI18n({
     locale: document.cookie.split('=')[1] || "EN",
@@ -22,5 +22,4 @@ const app = createApp(App)
 
 app.use(router)
 app.use(i18n)
-// app.use(ajax)
 app.mount('#app')
